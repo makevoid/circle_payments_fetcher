a4eeee21a1e4842f4a6bec99bea09f0da5a901e5
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2021-12-14 21:38:43 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ruby:3.0.3-alpine3.15
+
+RUN gem i excon -v 0.89.0
+
+WORKDIR /app
+
+ADD . ./
+
+ENV CIRCLE_API_KEY $CIRCLE_API_KEY
+
+
+CMD ruby circle_payments.rb
---
+++ circle_payments.rb
@@ -0,0 +1,54 @@
+require "yaml"
+require "excon"
+
+DEBUG = false
+
+CIRCLE_API_KEY = ENV["CIRCLE_API_KEY"]
+
+# puts "CIRCLE_API_KEY #{CIRCLE_API_KEY}" if DEBUG
+
+CIRCLE_API_HEADERS = {
+  "Accept" => "application/json",
+  "Authorization" => "Bearer #{CIRCLE_API_KEY}"
+}
+
+CIRCLE_PAYMENTS_IDS_FILE_PATH = "./data/circle_payment_ids.txt"
+
+CIRLE_PAYMENTS_API_URL = "https://api.circle.com/v1/payments"
+
+FAILED_REQUESTS = []
+
+def read_all_payment_ids
+  payment_ids = File.read CIRCLE_PAYMENTS_IDS_FILE_PATH
+  payment_ids.strip!
+  payment_ids = payment_ids.split "\n"
+  payment_ids.map { |payment_id| payment_id.strip }
+end
+
+def get_payment(circle_payment_id:)
+  url = "#{CIRLE_PAYMENTS_API_URL}/#{circle_payment_id}"
+  begin
+    res = Excon.get url, headers: CIRCLE_API_HEADERS
+    res = res.body
+    JSON.parse res
+  rescue Exception => err
+    FAILED_REQUESTS << circle_payment_id
+  end
+end
+
+def main
+  output = []
+  payment_ids = read_all_payment_ids
+  payment_ids.each_with_index do |payment_id, idx|
+    next if idx > 2
+
+    output << get_payment(circle_payment_id: payment_id)
+  end
+  output = {
+    circle_payments: output,
+    failed_requests: FAILED_REQUESTS,
+  }.to_yaml
+  puts output
+end
+
+main
